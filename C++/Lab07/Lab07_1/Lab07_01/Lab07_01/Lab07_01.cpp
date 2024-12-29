0ae3e358e93013e1474e30480ef1d2b45719467b
LAPTOP-23GRM7GN\Setthapong <setthapong.mana@gmail.com>
2024-12-29 10:24:46 +0700

    update code

--- C++/Lab07/Lab07_1/Lab07_01/Lab07_01/Lab07_01.cpp
+++ C++/Lab07/Lab07_1/Lab07_01/Lab07_01/Lab07_01.cpp
@@ -1,14 +1,23 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main() {
     int x = 10;
-    int* ptr = &x; // ???? address ??? x
+    int* ptr = &x; // เก็บ address ของ x
 
-    cout << "?????? x: " << x << endl;           // ?????????? x
-    cout << "?????????? x: " << &x << endl;    // ???? address ??? x
-    cout << "??????? ptr: " << *ptr << endl;   // ?????????????? ptr
-    cout << "????????? ptr: " << ptr << endl;  // ???? address ??? ptr ???????
+    cout << "ค่าของ x: " << x << endl;           // แสดงค่าของ x
+    cout << "ตำแหน่งของ x: " << &x << endl;    // แสดง address ของ x
+    cout << "ค่าผ่าน ptr: " << *ptr << endl;   // เข้าถึงค่าผ่าน ptr
+    cout << "ตำแหน่งใน ptr: " << ptr << endl;  // แสดง address ที่ ptr เก็บไว้
+
+    int y = 20;
+    ptr = &y; // เก็บ address ของ x
+
+    cout << "ค่าของ y: " << x << endl;           // แสดงค่าของ x
+    cout << "ตำแหน่งของ y: " << &x << endl;    // แสดง address ของ x
+    cout << "ค่าผ่าน ptr: " << *ptr << endl;   // เข้าถึงค่าผ่าน ptr
+    cout << "ตำแหน่งใน ptr: " << ptr << endl;  // แสดง address ที่ ptr เก็บไว้
+
 
     return 0;
 }
